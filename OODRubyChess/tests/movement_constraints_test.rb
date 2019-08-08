0e17333db5f22582fd549cb17ac0ee03f174a2a7
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2019-08-08 02:14:07 -0400

    Added board, piece, position, and a test test file

---
+++ OODRubyChess/tests/movement_constraints_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require './OODRubyChess/tests/test-helper'
+
+# this shiny device will run unit tests against our chess constraints system
+class MovementConstraintsTests < MiniTest::Unit::TestCase
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    'not implemented'
+  end
+end
