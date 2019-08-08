93a2ed85a3d67704e390f12a9ffdcdd23fb67525
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2019-08-08 03:42:30 -0400

    Some bugfix to the board following tests

--- OODRubyChess/src/board.rb
+++ OODRubyChess/src/board.rb
@@ -4,6 +4,8 @@ require './OODRubyChess/src/piece'
 require './OODRubyChess/src/position'
 # this shiny device handles generic displacements on a 8x8 chess board
 class Board
+  BASIC_BOUNDS = { lower_x: 0, lower_y: 0, upper_x: 7, upper_y: 7 }.freeze
+
   attr_accessor :upper_bound_x
   attr_accessor :upper_bound_y
   attr_accessor :lower_bound_x
@@ -12,22 +14,29 @@ class Board
   # describes a board error
   class BoardError < StandardError; end
 
+  # describes an OutOfBounds Error
+  class OutOfBoundsError < BoardError
+    def message
+      'The requested position is out of bounds.'
+    end
+  end
+
   # describes a piece placing error
-  class BoardPlaceError < BoardError
+  class OccupiedError < BoardError
     def message
       'There is already a piece here.'
     end
   end
 
   # describes a piece removing error
-  class BoardRemoveError < BoardError
+  class EmptyError < BoardError
     def message
       'There is no piece to remove here.'
     end
   end
 
   # creates an empty board
-  def initialize(**bounds)
+  def initialize(bounds)
     self.lower_bound_x = bounds[:lower_x]
     self.lower_bound_y = bounds[:lower_y]
     self.upper_bound_x = bounds[:upper_x]
@@ -36,15 +45,18 @@ class Board
   end
 
   def validate_positions(*positions)
+    ret = true
     positions.each do |pos|
-      false unless validate_position_in_bounds(pos)
+      unless validate_position_in_bounds(pos)
+        ret = false
+        break
+      end
     end
-    true
+    ret
   end
 
   def validate_position_in_bounds(position)
-    (position.x >= lower_bound_x && position.x <= upper_bound_x) \
-    && (position.y >= lower_bound_y && position.y <= lower_bound_y)
+    (position.x >= lower_bound_x && position.x <= upper_bound_x) && (position.y >= lower_bound_y && position.y <= upper_bound_y)
   end
 
   def position_occupied(position)
@@ -56,13 +68,15 @@ class Board
   end
 
   def place(piece, position)
-    raise BoardPlaceError if position_occupied(position)
+    raise OutOfBoundsError unless validate_position_in_bounds(position)
+    raise OccupiedError if position_occupied(position)
 
     @occupied_space[position.x][position.y][:piece] = piece
   end
 
   def remove(position)
-    raise BoardRemoveError unless position_occupied(position)
+    raise OutOfBoundsError unless validate_position_in_bounds(position)
+    raise EmptyError unless position_occupied(position)
 
     @occupied_space[position.x][position.y][:piece] = nil
   end
---
+++ OODRubyChess/tests/board_test.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require './OODRubyChess/tests/test_helper'
+require './OODRubyChess/src/board'
+
+# this shiny device will run unit tests against our chess constraints system
+class BoardUnitTests < MiniTest::Test
+  def setup
+    @board = Board.new(Board::BASIC_BOUNDS)
+    @random_valid_position = Position.new(rand(0..7), rand(0..7))
+    @other_random_valid_position = Position.new(rand(0..7), rand(0..7))
+    @random_invalid_x_position = Position.new(rand(8..16), rand(7))
+    @random_invalid_y_position = Position.new(rand(7), rand(8..16))
+
+    @fixed_position35 = Position.new(3, 5)
+    @fixed_position76 = Position.new(7, 6)
+
+    @some_piece = Piece.new(Piece::Type::KING, Piece::Color::WHITE)
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_validate_position
+    assert_equal(true, @board.validate_position_in_bounds(@random_valid_position))
+    assert_equal(true, @board.validate_position_in_bounds(@other_random_valid_position))
+    assert_equal(false, @board.validate_position_in_bounds(@random_invalid_x_position))
+    assert_equal(false, @board.validate_position_in_bounds(@random_invalid_y_position))
+  end
+
+  def test_validate_many_positions
+    assert_equal(true, @board.validate_positions(@random_valid_position, @other_random_valid_position))
+    assert_equal(false, @board.validate_positions(@random_valid_position, @random_invalid_x_position))
+    assert_equal(false, @board.validate_positions(@random_invalid_x_position, @random_invalid_y_position))
+  end
+
+  def test_position_occupied
+    assert_equal(false, @board.position_occupied(@random_valid_position))
+    @board.place(@some_piece, @random_valid_position)
+    assert_equal(true, @board.position_occupied(@random_valid_position))
+  end
+
+  def test_piece_from_position
+    assert_nil(@board.get_piece_from_position(@random_valid_position))
+    @board.place(@some_piece, @random_valid_position)
+    assert_equal(@some_piece, @board.get_piece_from_position(@random_valid_position))
+  end
+
+  def test_place_piece
+    assert_raises Board::OutOfBoundsError do
+      @board.place(@some_piece, @random_invalid_y_position)
+    end
+    @board.place(@some_piece, @random_valid_position)
+    assert_equal(@some_piece, @board.get_piece_from_position(@random_valid_position))
+    assert_raises Board::OccupiedError do
+      @board.place(@some_piece, @random_valid_position)
+    end
+  end
+
+  def test_remove_piece
+    assert_raises Board::OutOfBoundsError do
+      @board.remove(@random_invalid_x_position)
+    end
+    assert_raises Board::EmptyError do
+      @board.remove(@random_valid_position)
+    end
+    @board.place(@some_piece, @random_valid_position)
+    @board.remove(@random_valid_position)
+    assert_nil(@board.get_piece_from_position(@random_valid_position))
+  end
+
+  def test_serialize
+    'not implemented'
+  end
+end
